3090db2a617b70023ab009db7fc44c9826fb6cf5
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-29 22:27:02 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,3 +11,13 @@ class MusicLibraryController
   end
   
 end
+
+# "Welcome to your music library!"
+# ("To list all of your songs, enter 'list songs'."
+# "To list all of the artists in your library, enter 'list artists'."
+# "To list all of the genres in your library, enter 'list genres'."
+# "To list all of the songs by a particular artist, enter 'list artist'."
+# "To list all of the songs of a particular genre, enter 'list genre'."
+# "To play a song, enter 'play song'."
+# ("To quit, type 'exit'."
+# "What would you like to do?"
