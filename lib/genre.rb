b1c58ffa187b98a92a6b4cf36cf757a4f9039644
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-29 20:16:25 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,39 @@
+class Genre 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all = []
+    @songs = []
+  end
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end 
+  
+  def songs
+    @songs 
+  end
+  
+  def artists
+    self.songs.collect { |song| song.artist}.uniq
+  end
+  
+end 
