06e5812f0979ee7ca9f5f8d9d81ebe0c6c399000
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-29 20:53:35 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,4 +4,8 @@ module Concerns::Findable
     self.all.detect { |element| element.name == name }
   end
   
+  def find_or_create_by_name(name)
+    self.all.detect { |element| element.name == name } || self.new
+  end
+  
 end
