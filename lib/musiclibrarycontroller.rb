b973e763a496d59a201dd0f3247092d97132a672
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-29 23:35:28 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+    Song.all.sort! {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
     puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end
   end
