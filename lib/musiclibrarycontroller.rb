067bb6904a689a7992e1492d6eff06cf436b7572
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 00:29:10 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -52,7 +52,7 @@ class MusicLibraryController
     end
     
     def list_genres
-      Genres.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |genre, i|
+      Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |genre, i|
         puts "#{i}. #{genre.name}"
       end
     end
