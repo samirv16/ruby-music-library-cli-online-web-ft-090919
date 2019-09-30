bb31859a5f971804768ecf20d5748a175d1198e9
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 01:04:21 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
   end
   
   def play_song
-    puts "Which song would you like to play?"
+    puts "Which song number would you like to play?"
     input = gets.chomp
   end 
   
